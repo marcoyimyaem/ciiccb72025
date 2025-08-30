8f61cb508f0b94f31c2b8127adb8315e4f8808e9
marcoyimyaem <marcoyimyaem@gmail.com>
2025-08-30 15:08:16 +0000

    Day 17

---
+++ chapter3/StringLesson.java
@@ -0,0 +1,28 @@
+package chapter3;
+
+public class StringLesson {
+    public static void main(String[] args) {
+        String name = "Fluffy";
+        name = "Floofy";
+        short q = 'a';
+        String name2 = "Fluffy"; 
+        char xp = 256;
+        System.out.println(xp);
+         String a = "1";
+        a = "0";
+        a+="2";
+        a+="3";
+        String b = "1";
+        name = null;
+        name2 = null;
+        String name3 = new String("fluffy");
+        System.out.println(a);
+        String word1 = "Stand alone";
+        System.out.println(word1.toUpperCase());
+        word1= word1.toUpperCase();
+        System.out.println(word1.toLowerCase());
+        System.out.println(word1);
+        String trimSample = "       \t \n \t Marc Yim          \t \n";
+        System.out.println(trimSample.trim());
+    }
+}
